1c57b4f4a7f37de1ccd2d748ce6b4f01299b3fae
finscn <finscn@gmail.com>
2014-07-09 12:17:38 +0800

    first commit

---
+++ Poorplate.js
@@ -0,0 +1,80 @@
+"not use strict mode";
+
+var Poorplate = function(options) {
+    for (var p in options) {
+        this[p] = options[p];
+    }
+};
+
+Poorplate.prototype = {
+    constructor: Poorplate,
+
+    START: "{{",
+    END: "}}",
+    SCRIPT: "@",
+
+    cache: {},
+    updateDom: function(dom, data, noneValue) {
+        var content = dom.innerHTML;
+        var key = dom.id || content;
+        var compiledTmpl = this.cache[key] || this.compileTemplate(content);
+        var newContent = this.runTemplate(compiledTmpl, data, noneValue);
+        dom.innerHTML = newContent;
+    },
+
+    compileTemplate: function(template) {
+        var startLen = this.START.length;
+        var endLen = this.END.length;
+        var scriptLen = this.SCRIPT.length;
+
+        var compiledTmpl = [];
+        var current = 0;
+        while (true) {
+            var startIdx = template.indexOf(this.START, current);
+            var begin = startIdx + startLen;
+            var endIdx = template.indexOf(this.END, begin);
+            if (begin >= startLen && endIdx > begin) {
+                compiledTmpl.push(template.substring(current, startIdx));
+                var dynamic = template.substring(begin, endIdx);
+                if (dynamic.indexOf(this.SCRIPT) === 0) {
+                    compiledTmpl.push({
+                        script: dynamic.substring(scriptLen)
+                    });
+                } else {
+                    compiledTmpl.push({
+                        dynamic: dynamic
+                    });
+                }
+            } else {
+                compiledTmpl.push(template.substring(current));
+                break;
+            }
+            current = endIdx + endLen;
+        }
+        return compiledTmpl;
+    },
+
+    runTemplate: function(compiledTmpl, data, noneValue) {
+        if (typeof compiledTmpl == "string") {
+            compiledTmpl = this.compileTemplate(compiledTmpl);
+        }
+        data = data || {};
+        noneValue = noneValue || noneValue === 0 || noneValue === false ? noneValue : '';
+        var rs = [];
+        for (var i = 0, len = compiledTmpl.length, sIdx = 0; i < len; i++) {
+            var f = compiledTmpl[i];
+            if (f.dynamic) {
+                var v = data[f.dynamic];
+                rs[i] = v === null || v === undefined ? noneValue : v;
+            } else if (f.script) {
+                var _s = undefined;
+                var script = "with(data){ _s=" + f.script + " }";
+                eval(script);
+                rs[i] = _s;
+            } else {
+                rs[i] = f;
+            }
+        }
+        return rs.join('');
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,115 @@
-PoorPlate
+Poorplate
 =========
 
 a simple JS template toolkit
+
+该工具非常简单, 就是把一段文本中的 `{{属性名}}` 替换成`数据对象`中相应的属性值
+
+当然 , {{ 和 }} 这两个标志符是可以自定义的.
+
+
+======================
+
+## 简单示例
+
+
+
+template字符串
+
+```
+        Name: {{name}}        
+        Age: {{age}}
+        Gender: {{gender}}
+```
+
+数据对象
+
+```
+    var data = {
+            name: "Rocky",
+            age: "20",
+            gender: "m"
+        };
+```
+
+执行替换
+
+```
+var pp= new Poorplate();
+// 预编译模板
+var compiled = pp.compileTemplate(template);
+// 执行编译后的模板
+var newText = pp.runTemplate(compiled, data);
+```
+
+此时 newText 为
+
+```
+        Name: Rocky        
+        Age: 20
+        Gender: m
+```
+
+
+## 稍微高级一点示例
+
+`runTemplate`函数有第三个参数: 当属性值为null 或 undefined 时, 显示什么
+
+
+数据对象
+
+```
+// gender 为 null
+    var data = {
+            name: "Rocky",
+            age: "20",
+            gender: null
+        };
+```
+
+执行替换
+
+```
+var pp= new Poorplate();
+// 预编译模板
+var compiled = pp.compileTemplate(template);
+// 执行编译后的模板, 注意看第三个参数
+var newText = pp.runTemplate(compiled, data, "--Unknown--");
+```
+
+此时 newText 为
+
+```
+        Name: Rocky        
+        Age: 20
+        Gender: --Unknown--
+```
+
+
+## 更高级一点示例
+
+模板中可以包含简单的代码和表达式
+
+比如这样: (注意紧接着 {{ 的那个`@`
+
+```
+     Title: {{@ gender=="m"?"Mr":"Miss" }}
+     Time: {{@ foo(data) }}
+```
+
+gender 为data对象的属性, 表达式为标准的js表达式
+foo 为在外部定义的函数, 函数的返回值 会替换
+
+====================
+
+更多信息详见代码 和 example.html
+
+
+
+.
+.
+.
+
+
+
+
